dc8f492115741c8e3eb33089cdccb7ec7c6f7e68
Matthew Leach <matthew@mattleach.net>
2020-06-18 20:18:50 +0100

    quadrant.h: new
    Move quadrant declarations into a seperate header file.

--- main.cpp
+++ main.cpp
@@ -5,18 +5,7 @@
 #include <sstream>
 #include <stdexcept>
 
-enum class ITEQuadrant
-{
-    FAR_LEFT = 1,
-    LEFT = 2,
-    RIGHT = 3,
-    FAR_RIGHT = 4,
-};
-
-constexpr std::array<ITEQuadrant, 4> allQuadrants = {ITEQuadrant::FAR_LEFT,
-                                                     ITEQuadrant::LEFT,
-                                                     ITEQuadrant::RIGHT,
-                                                     ITEQuadrant::FAR_RIGHT};
+#include "quadrant.h"
 
 class ITEDevice
 {
