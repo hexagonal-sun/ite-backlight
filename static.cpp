6b4021bcd14311510bad2fa967a11dd9f540724a
Matthew Leach <matthew@mattleach.net>
2020-06-20 21:45:17 +0100

    static: add -r option
    Add a -r option which allows the colour specification to be continually
    read from STDIN.

--- static.cpp
+++ static.cpp
@@ -3,25 +3,66 @@
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
+#include <getopt.h>
+#include <string>
+#include <boost/algorithm/string.hpp>
 
 #include "ite-device.h"
 
 void usage(const char *progname)
 {
-    std::cerr << "Usage: " << progname << " COLOUR COLOUR COLOUR COLOUR\n";
+    std::cerr << "Usage: " << progname << " [-r] COLOUR COLOUR COLOUR COLOUR\n";
     std::cerr << "\n";
     std::cerr << "Set the provided colours across the keyboard in the\n";
     std::cerr << "respective order: far left, left, right, far right.\n";
     std::cerr << "\n";
+    std::cerr << "-r: When set continually read COLOURs from STDIN and set\n";
+    std::cerr << "    after each newline.  When -r is specified no COLOURs\n";
+    std::cerr << "    should be specified.\n";
+    std::cerr << "\n";
     Colour::printUsage(std::cerr);
+}
+
+void setStaticColours(ITEDevice &device,
+                      const std::string &c1,
+                      const std::string &c2,
+                      const std::string &c3,
+                      const std::string &c4)
+{
+    Colour colour_fl(c1);
+    Colour colour_l (c2);
+    Colour colour_r (c3);
+    Colour colour_fr(c4);
+
+    device.setStaticStyle({colour_fl, colour_l,
+                           colour_r, colour_fr}, ITEBrightness::VERY_BRIGHT);
+
 }
 
 int main(int argc, char *argv[])
 {
     libusb_context *ctx;
+    bool readFromStdin = false;
 
-    if (argc != 5) {
-        std::cerr << "Error: expected exactly four arguments.\n";
+
+    while (true) {
+        const auto opt = getopt(argc, argv, "r");
+
+        if (opt == -1)
+            break;
+
+        switch (opt) {
+            case 'r':
+                readFromStdin = true;
+                break;
+            default: /* '?' */
+                usage(argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (!readFromStdin && argc - optind != 4) {
+        std::cerr << "Error: expected exactly four colours.\n";
         usage(argv[0]);
         return 1;
     }
@@ -41,13 +82,32 @@ int main(int argc, char *argv[])
 
     try {
         ITEDevice iteDev(ctx);
-        Colour colour_fl(argv[1]);
-        Colour colour_l(argv[2]);
-        Colour colour_r(argv[3]);
-        Colour colour_fr(argv[4]);
 
-        iteDev.setStaticStyle({colour_fl, colour_l,
-                               colour_r, colour_fr}, ITEBrightness::VERY_BRIGHT);
+        if (readFromStdin) {
+            std::string line;
+            while (std::getline(std::cin, line)) {
+                std::vector<std::string> colours;
+
+                boost::algorithm::trim(line);
+
+                boost::algorithm::split(colours, line, boost::is_any_of(" "));
+
+                if (colours.size() != 4)
+                    std::cerr << "Warning string \"" << line << "\" does not specify four colours. Skipping\n";
+
+                setStaticColours(iteDev,
+                                 colours[0],
+                                 colours[1],
+                                 colours[2],
+                                 colours[3]);
+            }
+        } else {
+            setStaticColours(iteDev,
+                             argv[optind],
+                             argv[optind + 1],
+                             argv[optind + 2],
+                             argv[optind + 3]);
+        }
 
     } catch (std::invalid_argument &e) {
         std::cerr << "Error: " << e.what() << "\n";
